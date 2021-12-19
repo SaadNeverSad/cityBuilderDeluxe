84fda3f845ceb58efde9c471505caafd375a0b09
Saad AIT CHIKH <saad.ait-chikh@insa-rennes.fe>
2021-12-19 22:45:37 +0100

    scoreboard style

--- game-frontend/src/app/component/replay/replay.component.ts
+++ game-frontend/src/app/component/replay/replay.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { UndoHistory } from 'interacto';
+import { AddBlock } from 'src/app/command/AddBlock';
+import { Undoable } from 'src/app/model/undoable';
 
 @Component({
   selector: 'app-replay',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReplayComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  undos: readonly Undoable[];
+  
+  constructor(public undoHistory: UndoHistory,private http: HttpClient) {
+    this.undos = undoHistory.getUndo();   
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  public postReplay(): Promise<void> {
+    return this.http
+    // You can (must) join data to your post queries.
+    // Use JSON.stringify to marshall data
+    .post<void>('game/replay', JSON.stringify(this.undos))
+    .toPromise();
+  }
+  
+     
 }
